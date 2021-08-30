c7b92f64ea4a9760c6087f595f489ba792e18f72
chefkitten <dgdev@naver.com>
2021-08-30 17:30:44 +0900

    send data test

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const options = {
+  method: 'GET',
+  url: 'https://unogsng.p.rapidapi.com/genres',
+  headers: {
+    'x-rapidapi-host': 'unogsng.p.rapidapi.com',
+    'x-rapidapi-key': '412cd19b01msh0b3fcf00757d8d6p1f9bedjsnf7eed9375b4a',
+    useQueryString: true
+  }
+};
+
+
+router.get('/',async(req,res)=>{ 
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+    let data=[];
+    data=JSON.parse(body);
+    res.send(data);
+  });
+  
+}); 
 
 module.exports = router;
+
+
